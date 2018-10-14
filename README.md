33df786b5723a8e45f7441fd0d32e7148a183413
Sumanth <DevarakondaSumanth@JohnDeere.com>
2018-10-14 21:54:15 +0530

    all http verbs

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 - RESTful services using node and express
 - nodemon
 - run on port taken from environment variable
+- joi for validating requests
+- object destructuring
 
 
 ### Notes
@@ -12,3 +14,4 @@
 `npm init --yes` to get package.json
 `npm i express` to get express installed
 `npm i -g nodemon` need not restart the server always. run with nodemon
+`npm i joi` to get joi
--- handle-requests/index.js
+++ handle-requests/index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');//returns class; so upper case Joi
 const express = require('express');
-const app = express(); //returns obj of type express; by convention we call it app.
+const app = express();
+
+app.use(express.json());//to parse json coming from request
 
 const courses = [
     { id: 1, name: 'course 1'},
@@ -8,17 +11,74 @@ const courses = [
 ];
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
-})
+    res.send(courses);
+});
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with given id is not found');
+    if (!course) return res.status(404).send('The course with given id is not found');
     res.send(course);
-})
+});
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    //if (!req.body.name || req.body.name.length < 3) {
+    //    res.status(400).send('Name is reqd and should be minimum 3 chars');
+    //    return;
+    //}
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with given id is not found');
+
+    //const result = validateCourse(req.body);
+    //if (result.error) {...}
+    const { error } = validateCourse(req.body); //~ result.error ; obj destructuring in adv js
+    if (error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    console.log(course);
+    if (!course) return res.status(404).send('The course with given id is not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+};
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-
-//start from handling post request
